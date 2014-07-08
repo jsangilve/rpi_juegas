6449482397b15f464dee2bba17f00001adf6f064
Jose San Gil <jsangil@gmtprime.com>
2014-07-08 18:32:38 -0400

    Added twitter variables and updated images

--- juegas.py
+++ juegas.py
@@ -6,8 +6,8 @@ import random
 
 # initialize GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN) # right button
-GPIO.setup(24, GPIO.IN) # left button
+GPIO.setup(23, GPIO.IN)  # right button
+GPIO.setup(24, GPIO.IN)  # left button
 
 pygame.init()
 
@@ -18,7 +18,7 @@ pygame.display.set_caption('Juegas?')
 games = {
     '000': ['Twister', 'La Rayuela'],
     '001': ['Basket'],
-    '010': ['Chocolate Ingles', 'Las Sillas', 'La Rayuela', 'Twister', 'Basket'],
+    '010': ['Chocolate Ingles', 'Las Sillas', 'La Rayuela', 'Twister'],
     '011': ['Voleibol', 'Basket'],
     '100': ['Twister'],
     '101': ['Frontenis', 'Badminton', 'Basket'],
@@ -47,26 +47,46 @@ images['question3'] = pygame.image.load('img/question3.png')
 images['question3l'] = pygame.image.load('img/question3l.png')
 images['question3r'] = pygame.image.load('img/question3r.png')
 
+# Twitter
+tweets = {
+    '000': u"Dos peques divirtiéndose en el río jugando %s. Vivan los juegos tradicionales!",
+    '001': u"Duelo entre amigos! Un par de chicos se enfrentan en la cancha de %s. Ánimo!",
+    '010': u"Un grupo de chicos pasándola bien mientras juegan %s. Apostamos por los juegos tradicionales!",
+    '011': u"Tiempo de competencia! Un grupo de chicos se enfrentan en la cancha de %s. Suerte a ambos!",
+    '100': u"Un par de adultos quieren volver a su infancia. Habrá un enredo en la grama mientras juegan %s",
+    '101': u"Tiempo de duelo! Se vienen unos minutos tensos para dos amigos que se enfrentan en %s. Suerte!",
+    '110': u"Volvamos todos a divertirnos como los chicos! Veamos cómo la pasa este grupo jugando %s",
+    '111': u"A los grandes les tocó el %s. Vayamos a apoyar a los equipos al Río Besós!",
+}
+
+CONSUMER_KEY = 'edEuKhTgoFBvmrybY5zKjdiJG'
+CONSUMER_KEY_SECRET = ''
+ACCESS_TOKEN = '2608405993-OVYhnNzqqW6kmsuOBO2RUaCUreDMw6Hp1veBPJj'
+ACCESS_TOKEN_SECRET = ''
+
 current = 0
 start = True
 waiting = False
 loading = True
 result = []
 
+
 def restart():
     global current, start, waiting, loading, result
     current = 0
     start = True
     waiting = False
-    loading= True
+    loading = True
     result = []
 
+
 def choose_game(choices):
-    selected = random.randint(0,len(choices)-1)
+    selected = random.randint(0, len(choices) - 1)
     return choices[selected]
 
+
 def show_image(name):
-    screen.blit(images[name], (0,0))
+    screen.blit(images[name], (0, 0))
 
 while True:
     left = GPIO.input(24)
@@ -106,20 +126,19 @@ while True:
             waiting = False
             result.append(1)
     elif current == 4:
-       if loading:
-         key = ''.join([str(x) for x in result]) 
-         game = choose_game(games[key])
-         loading = False
-         show_image('loading')
-       else:
-         time.sleep(2)
-         print "El juego es *" + game + "*"
-         show_image(game)
-         current = current + 1
+        if loading:
+            key = ''.join([str(x) for x in result])
+            game = choose_game(games[key])
+            loading = False
+            show_image('loading')
+        else:
+            time.sleep(2)
+            print "El juego es *" + game + "*"
+            show_image(game)
+            current = current + 1
     elif current > 4:
         restart()
         time.sleep(25)
-    
-    pygame.display.update()
-    time.sleep(.1) # to detect buttons
 
+    pygame.display.update()
+    time.sleep(.1)  # to detect buttons
